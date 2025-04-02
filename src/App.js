527f13b31bdc8e9b19446486902889bd132ebdb7
gene-999 <eugenebaidoo5@gmail.com>
2025-04-02 10:10:13 +0000

    minor ui improvement

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ export default function App() {
           </a>
         </nav>
         <div className="">
-        <button className="bg-slate-900 px-8 text-s h-fit rounded-full hover:bg-gray-700">
-          Download App
-        </button>
+          <button className="bg-slate-900 py-1 text-s h-fit rounded-full hover:bg-gray-700">
+            Download App
+          </button>
         </div>
       </header>
 
@@ -31,7 +31,7 @@ export default function App() {
         <div className="flex flex-col gap-4 basis-1/2">
           <span className="text-sm">Sales Analytics</span>
           <div className="mb-6">
-            <span className="text-4xl md:text-6xl md:font-bold lg:text-6xl lg:font-bold font-semibold">
+            <span className="text-4xl md:text-6xl font-bold">
               We Make &{" "}
               <img
                 className="inline-block w-8 md:w-12 lg:w-12 pb-2"
@@ -44,7 +44,7 @@ export default function App() {
           </div>
           <div className="border-b-[1.5px] border-black flex justify-between mt-16 mr-8">
             <span className="font-medium">BOOK A CONSULTATION</span>
-            <span className="order-2">
+            <span className="justify-end">
               <MailIcon />
             </span>
           </div>
@@ -53,25 +53,34 @@ export default function App() {
           <img src={require("./hombre.png")} />
         </div>
       </div>
-      <div className="flex flex-row gap-3 mt-2">
-        <div className="bg-slate-800 rounded-lg basis-4/6 py-2 px-2">
-          <div className="flex justify-between">
-            <div className="self-end bg-slate-800 text-white rounded-full px-2 py-0.5 h-6 border text-xs font-normal">
-              Track Sales
-            </div>
-            <div className="px-1 top-5 right-5">
-              <div>
-                <ArrowOutwardIcon style={{ color: "white" }} />
+      <div className="flex flex-row gap-3 mt-2 pb-2">
+        <div className="flex basis-4/6 my-2 px-2 w-9">
+          <div className="w-full">
+            <div className="flex justify-between bg-slate-800 rounded-r-2xl rounded-tl-xl pt-0">
+              <div className="flex justify-between bg-slate-800 px-4 rounded-md w-96 p-0">
+                <div className="self-end bg-slate-800 text-white rounded-full px-2 py-[3px] mb-2 h-6 border-[0.25px] text-xs font-normal">
+                  Track Sales
+                </div>
+              </div>
+              <div className="bg-slate-900 rounded-bl-2xl rounded-ss-sm rounded-tr-2xl">
+                <div className="px-1 place-self-end -mr-1 right-5 -mt-1 mb-2">
+                  <div className="p-2 bg-slate-800  rounded-xl m-1">
+                    <ArrowOutwardIcon
+                      fontSize="medium"
+                      style={{ color: "white" }}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div>
-            <img src={require("./graph.png")} />
+            <div className="bg-slate-800 p-4 rounded-tr-xl rounded-b-lg">
+              <img src={require("./graph.png")} className="rounded-lg " />
+            </div>
           </div>
         </div>
-        <div className="bg-red-300 rounded-2xl w-52 py-5 px-2 flex flex-col gap-9 my-2 ml-10">
-          <div className="flex justify-between">
-            <div>
+        <div className="bg-red-300 rounded-2xl w-52 py-5 px-2 flex flex-col gap-9 my-2">
+          <div className="flex justify-between p-2 py-2.5">
+            <div className="">
               <PlayCircleIcon style={{ color: "white" }} fontSize="large" />
             </div>
             <div className="order-last flex flex-row px-2">
@@ -85,15 +94,15 @@ export default function App() {
               />
             </div>
           </div>
-          <text className="font-normal text-xs mt-2">
+          <text className="font-normal text-xs mt-2 order-last">
             Business lectures
             <br />
             <span className="font-bold text-4xl">400+</span>
           </text>
         </div>
         <div className="bg-blue-400 rounded-2xl py-3 w-52 pl-2 px-2 flex flex-col gap-9 my-2">
-          <div className="flex justify-between">
-            <div className="self-end bg-black text-white rounded-full pr-2 hover:bg-slate-600">
+          <div className="flex justify-between py-2.5">
+            <div className="self-end bg-black text-white rounded-full hover:bg-slate-600 p-0.5">
               <ShowChartIcon
                 style={{
                   margin: "1px",
@@ -106,7 +115,7 @@ export default function App() {
                 }}
                 fontSize=""
               />
-              <text className="ml-1 text-xs">34 metrics</text>
+              <text className="ml-1 text-xs p-1">34 metrics</text>
             </div>
             <div className="order-last flex flex-row px-2">
               <div>
